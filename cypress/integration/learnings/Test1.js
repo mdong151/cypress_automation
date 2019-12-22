12c894d79b06a229af56309e07c4d38c48eaf866
mdong151 <mdong151@gmail.com>
2019-12-22 22:35:26 +0700

    Learn methods find, should, eq, contains, each

--- cypress/integration/learnings/Test1.js
+++ cypress/integration/learnings/Test1.js
@@ -1,6 +1,18 @@
+/// <reference types="cypress" />
 describe('My First Test Suite', function () {
     it('My first test case', function () {
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.get('div.products-wrapper div.product:visible').should('have.length', 4)
+        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+
+            const productName = $el.find('.product-name').text()
+            if (productName.includes('Cashews')) {
+                $el.find('button').click();
+            }
+        })
     })
 })
 
